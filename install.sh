cb3395548ed2b17748f2b3b3257bd7d6b13a5c01
tbf5005 <103283645+tbf5005@users.noreply.github.com>
2024-03-06 13:23:24 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+clear
+
+cp -r app/* /var/bot/
+
+udpport=7302
+echo -e "\nPlease input UDPGW Port ."
+printf "Default Port is \e[33m${udpport}\e[0m, let it blank to use this Port: "
+read udpport
+
+sed -i "s/PORT_UDPGW=.*/PORT_UDPGW=$udpport/g" /var/tbf/.env
+apt update -y
+apt install git cmake -y
+git clone https://github.com/ambrop72/badvpn.git /root/badvpn
+mkdir /root/badvpn/badvpn-build
+cd  /root/badvpn/badvpn-build
+cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 &
+wait
+make &
+wait
+cp udpgw/badvpn-udpgw /usr/local/bin
+cat >  /etc/systemd/system/videocall.service << ENDOFFILE
+[Unit]
+Description=UDP forwarding for badvpn-tun2socks
+After=nss-lookup.target
+
+[Service]
+ExecStart=/usr/local/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:$udpport --max-clients 999
+User=videocall
+
+[Install]
+WantedBy=multi-user.target
+ENDOFFILE
+useradd -m videocall
+systemctl enable videocall
+systemctl start videocall
+sed -i "s/PORT_UDPGW=.*/PORT_UDPGW=$udpport/g" /var/tbf/.env
