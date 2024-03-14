19b105083931149138953907c14e6f4c56d1a5e0
tbf5005 <103283645+tbf5005@users.noreply.github.com>
2024-03-14 17:22:15 +0330

    Update addUser.sh

--- addUser.sh
+++ addUser.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# ایجاد نام کاربری
+read -p "لطفاً نام کاربری جدید را وارد کنید: " username
+sudo adduser $username
+
+# تنظیم اجازه SSH
+sudo usermod -aG ssh $username
+
+# تنظیم رمز عبور
+sudo passwd $username
+
+# نمایش نام کاربری و رمز عبور
+echo "نام کاربری: $username"
+read -s -p "لطفاً رمز عبور را وارد کنید: " password
+echo "رمز عبور: $password"
